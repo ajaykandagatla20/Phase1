df4011b021b2a67689ec4c2c12c9673553fa79be
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:18:00 +0530

    day2

---
+++ Day2/Mam example Arrays/Mam example Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+namespace ConsoleProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello");
+            Console.Write("Hi");
+            Console.Write("All");
+
+            string name = " C Sharp";
+
+            Console.WriteLine(name);
+            Console.WriteLine("Learning" + name);
+
+            //String Interpolation
+            Console.WriteLine($"Learning {name}");
+
+            Console.WriteLine("Enter a key");
+
+            int var1 = Console.Read(); // returns single value i.e the ascii value of a character
+            Console.WriteLine($"Ascii value of the key you have entered : {var1}");
+
+            Console.WriteLine("Enter Another key");
+
+            ConsoleKeyInfo var2 = Console.ReadKey(); // return multiple values or info of that key which you have entered 
+
+            Console.WriteLine($"\nThe key you have entered : {var2.Key} , KeyChar:{var2.KeyChar} ,ASCII value : {(int)var2.KeyChar}");
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Title = "Console Properties : ";
+
+            Console.WriteLine("BackgroundColor: Blue");
+            Console.WriteLine("ForegroundColor: White");
+            Console.WriteLine("Title : To see the title effect ");
+
+
+
+        }
+    }
+}
