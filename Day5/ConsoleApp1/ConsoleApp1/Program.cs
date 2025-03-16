045a68ac62bc298e506d78e1bddcd1c864256635
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:21:17 +0530

    day5

---
+++ Day5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+public class Class2
+{
+    public Class2()
+    {
+
+        Hashtable employee = new Hashtable(); //  initial capacity ,  load factor  , hashcode provider and comparer
+        employee.Add("ID", "565656");
+
+        employee.Add("Name", "Rohan");
+
+        employee.Add("Company", "Wipro");
+        employee.Add("Salary", "10LPA");
+        foreach (object key in employee.Keys)
+        {
+            Console.WriteLine(hashtable[key]);
+
+        }
+
+        foreach (DictionaryEntry items in hashtable)
+        {
+
+            Console.WriteLine($"{items.Key} , {items.Value}");
+        }
+
+        var cities = new Hashtable()
+        {
+            { "UK" , "London , Manchester"},
+            { "USA" , "Chicago, New Yord"},
+            { "India" , "Mumbai , Delhi"}
+
+        };
+
+
+
+    }
+
+
+}
+
+
---
+++ Day5/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Practice
+{
+    class first
+    {
+        static void Main(String[] args)
+        {
+
+            Second s = new Second("blue", 4);
+            Console.WriteLine(s.ToString());
+        }
+    }
+    class Second
+    {
+        string color;
+        int wheels;
+        public Second(string Color, int Wheels)
+        {
+            this.color = Color;
+            this.wheels = Wheels;
+        }
+        public override string ToString()
+        {
+            return "This is for practice";
+        }
+    }
+}
