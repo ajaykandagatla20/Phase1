6627507bde940bbc14e7e1ab3801ef79e75c8277
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:22:26 +0530

    day6

---
+++ Day6/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace ExceptionExample
+{
+
+    public class ExceptionPractive : Exception
+    {
+        public override string Message
+        {
+            get { return "Yor are not eligible to vote idiot!"; }
+        }
+    }
+
+
+    class program
+    {
+
+        public static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("Enter your name :");
+                string Name = Console.ReadLine();
+                Console.WriteLine("Enter your age :");
+                int Age = Convert.ToInt32(Console.ReadLine());
+                
+                if(Age<18)
+                {
+                    throw new NotEligibleToVoteException();
+                   
+                }
+            }
+            catch(NotEligibleToVoteException nv)
+            {
+                Console.WriteLine(nv.Message);
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Day6/Interface/Interface/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+/*
+class Person
+
+{
+
+   public string name
+    {
+        get { return name; }
+        set
+        {
+         name = value;
+        }
+    }
+
+
+    private int age;
+    public Person(string name, int age) // Constructor
+
+    {
+        this.name = name;
+        
+
+        this.age = age;
+
+    }
+    public int Age
+    {
+        get { return age; }
+
+        set { age = value; }
+
+    }
+
+}
+ */
+class first
+{
+    public static void Main(string[] args)
+    {
+        /*
+                Person person = new Person("John", 30); // Creates a new Person object using the constructor
+
+                Console.WriteLine(person.name); // Accesses the name using the getter
+
+                person.name = "Ajay"; // Modifies the age using the setter
+                Console.WriteLine(person.name);
+        
+
+
+        int? nullableInt = 15;
+
+        if (nullableInt.HasValue)
+        {
+            Console.WriteLine("The value is " + nullableInt.Value);
+        }
+        else
+        {
+            Console.WriteLine("The value is null");
+        }
+
+        */
+
+        string name = "Ajay Kumar Kandagatla";
+
+        Console.WriteLine(name.Substring(3,5));
+        Console.WriteLine(name.ToUpper());
+        Console.WriteLine(name.Length);
+        name += "Ajay";
+
+        Console.WriteLine(name);
+    }
+}
