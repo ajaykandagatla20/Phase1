eaf2af031d158e4a8eaabca3fffc3f802d8902a5
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:02:41 +0530

    Day1

---
+++ AreaOfRectangle/Console/Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Practice
+{
+    class first
+    {
+        static void Main(String[] args)
+        {
+
+            Second s = new Second("blue", 4);
+            string s1 = s.ToString();
+            Console.WriteLine(s1);
+        }
+    }
+    class Second
+    {
+        string color;
+        int wheels;
+        public Second(string Color, int Wheels)
+        {
+            this.color = Color;
+            this.wheels = Wheels;
+        }
+        public void Splay()
+        {
+           Console.WriteLine("This is for practice");
+        }
+
+    }
+}
