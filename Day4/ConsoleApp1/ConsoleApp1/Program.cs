755e7cb20ce6a86475e7115683c79d9ad0be3c7f
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:20:07 +0530

    day4

---
+++ Day4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Project
+{
+    class checkingEligiblity
+    {
+        void Age() {
+            if (age > 18)
+            {
+                Console.WriteLine("You are eligible to vote");
+            }
+            else
+                Cn
+
+
+
+        }
+                    
+                    
+    }
+
+
+
+
+
+
+    class voting
+    {
+
+
+    }
+}
