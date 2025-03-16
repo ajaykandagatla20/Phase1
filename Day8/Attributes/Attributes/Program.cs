74b93350b2b721078b778cb1955cc8d2eb8676b4
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:25:29 +0530

    day8

---
+++ Day8/Attributes/Attributes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(2, 3);
+            Add(new int[] { 5, 6, 5 });
+
+        }
+
+
+        public static void Add(int a,int b)
+        {
+            Console.WriteLine("result : "+(a+b));
+        }
+
+
+        public static int Add(int[] numbers)
+        {
+            int result = 0;
+            foreach(int n in numbers)
+            {
+                result = numbers[n];
+            }
+            return result;
+        }
+
+    }
+}
