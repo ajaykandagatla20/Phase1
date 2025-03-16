c72d32a2ea946b6607161b1585665569ac96e21e
Ajay Kumar Kandagatla <111872379+ajaykandagatla20@users.noreply.github.com>
2025-03-16 12:23:19 +0530

    day7

---
+++ Day7/Day7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Day7;
+
+namespace Day7
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Ajay Kumar Kandagatla";
+            int wordcount = word.GetWordCount();
+            Console.WriteLine("Total words in the given string is : "+wordcount);
+        }
+    }
+}
---
+++ Day7/Day7/StringCount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    static class StringCount
+    {
+        public static int GetWordCount(this string s)
+        {
+            if (!string.IsNullOrEmpty(s))
+            {
+                string[] str = s.Split(' ');
+                return str.Count();
+
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
